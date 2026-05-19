47b2c292759bee2d47021e87d8945745bb06ce96
404 Ameyy <139114740+Ameysr@users.noreply.github.com>
2026-05-19 22:30:43 +0530

    security: auto-fix Hardcoded Secret via Zerotrail AI

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-const JWT_SECRET = "super-secret-key-123";
+const JWT_SECRET = process.env.JWT_SECRET;
 export function authMiddleware(req: any, res: any, next: any) {
   const token = req.cookies.token;
   if (!token) return res.status(401).json({ error: "Unauthorized" });
